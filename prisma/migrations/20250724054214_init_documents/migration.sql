8a09d2a0d46fec83ee71cd0e17872c45f51d42b7
Safdari10 <zakersafdari@gmail.com>
2025-07-24 18:04:56 +1200

    feat: Add initial migration files for document model and PostgreSQL provider

---
+++ prisma/migrations/20250724054214_init_documents/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "document" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "version" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "document_pkey" PRIMARY KEY ("id")
+);
