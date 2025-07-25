cb0310158fba5eb42d1311ed579c244fb88c677f
Safdari10 <zakersafdari@gmail.com>
2025-07-25 18:45:14 +1200

    Add SendOperationParams type to types.ts for WebSocket operation payloads

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,14 +1,23 @@
+import { Operation } from "@/lib/ot";
+
 export type DocumentVersionSummary = {
   version: number;
   createdAt: string;
 };
 
-export interface VersionHistoryProp {
-  documentId: string;
-}
+export type SendOperationParams = {
+  ws: WebSocket;
+  docId: string;
+  op: Operation;
+  clientId: string;
+};
 
 export type DocumentVersionsRouteParams = {
   params: {
     id: string;
   };
 };
+
+export interface VersionHistoryProp {
+  documentId: string;
+}
